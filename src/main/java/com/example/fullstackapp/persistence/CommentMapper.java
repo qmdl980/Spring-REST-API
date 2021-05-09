916a9b1810286f8b9c8b9e1ff8a56f3aaea8e600
qmdl980 <qmdl98056@gmail.com>
2021-05-09 20:40:55 +0900

    210509 Comment 기능 추가

---
+++ src/main/java/com/example/fullstackapp/persistence/CommentMapper.java
@@ -0,0 +1,41 @@
+package com.example.fullstackapp.persistence;
+
+import com.example.fullstackapp.domain.CommentVO;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert({"<script>",
+            "INSERT INTO comment(content, board_id)",
+            "VALUES(#{content}, #{board_id})",
+            "</script>"})
+    int insertComment(CommentVO commentVO);
+
+    @Select({"<script>",
+            "SELECT * from comment",
+            "where id = #{id}",
+            "</script>"})
+    CommentVO findOneComment(int id);
+
+    @Select({"<script>",
+            "SELECT * from comment",
+            "order by id desc",
+            "</script>"})
+    List<CommentVO> findComment(int board_id);
+
+    @Update({"<script>",
+            "UPDATE comment",
+            "set content = #{content}",
+            "WHERE id = #{id}",
+            "</script>"})
+    int updateComment(CommentVO commentVO);
+
+    @Delete({"<script>",
+            "DELETE FROM comment",
+            "WHERE id = #{id}",
+            "</script>"})
+    int deleteComment(int id);
+}
